977e2587d1332219d31a24fcad50f726a1dc1e27
notypeboi <goodvvi@naver.com>
2020-05-01 00:50:50 +0900

    prisma

---
+++ src/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+import {GraphQLServer} from 'graphql-yoga'
+import logger from 'morgan';
+import schema from './schema'
+
+const PORT = process.env.PORT || 5000;
+
+const server = new GraphQLServer({schema})
+server.express.use(logger("dev"))
+server.start({port :PORT}, () => console.log(`Server running on port http://localhost:${PORT}`))
